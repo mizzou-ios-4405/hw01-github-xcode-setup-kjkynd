f112b480fa608a0f24db7be074121123e3d3a23a
kjkynd <kjkynd@missouri.edu>
2026-01-27 21:52:06 -0600

    Add Hello World app & the screenshot
    Add Hello World app & the screenshot

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by user288007 on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
